ad111b32f9dde477e2dd19d072384d514871ae1a
YeisonsUarez <42613235+YeisonsUarez@users.noreply.github.com>
2023-03-18 22:42:40 -0500

    Add swagger documentation

---
+++ src/main/java/com/example/recipes/config/DocumentationConfig.java
@@ -0,0 +1,25 @@
+package com.example.recipes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DocumentationConfig {
+
+    private static final String LOCAL_HOST = "http://localhost:8080";
+
+    @Bean
+    public OpenAPI api(
+            @Value("${info.tittle}") String tittle,
+            @Value("${info.description}") String description
+    ) {
+        Info info = new Info().title(tittle).description(description);
+        Server server = new Server().url(LOCAL_HOST);
+        return new OpenAPI().info(info).addServersItem(server);
+    }
+
+}
---
+++ src/test/java/com/example/recipes/integration/GenerateDocTest.java
@@ -0,0 +1,27 @@
+package com.example.recipes.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GenerateDocTest extends IntegrationTest {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void generateSwaggerDocumentation() throws IOException {
+        ResponseEntity<String> responseEntity = this.testRestTemplate.getForEntity("/v3/api-docs", String.class);
+        assertNotNull(responseEntity.getBody());
+        Path outputDir = Paths.get("docs");
+        Files.createDirectories(outputDir);
+        Files.writeString(outputDir.resolve("swagger.yaml"), responseEntity.getBody());
+    }
+}

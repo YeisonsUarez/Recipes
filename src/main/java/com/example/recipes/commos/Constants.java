b1701b2133489730a8d284e135abec82fb8d97ad
YeisonsUarez <42613235+YeisonsUarez@users.noreply.github.com>
2023-03-18 22:38:47 -0500

    add constants

---
+++ src/main/java/com/example/recipes/commos/Constants.java
@@ -0,0 +1,9 @@
+package com.example.recipes.commos;
+
+public interface Constants {
+
+    String NOT_FOUND = "Not found %s with id %s";
+
+    String DUPLICATE_DATA = "There is already a %s with the name";
+
+}

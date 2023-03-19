9d11e47690ce988214d9dd4e2ee76d5049ea384c
YeisonsUarez <42613235+YeisonsUarez@users.noreply.github.com>
2023-03-18 22:30:27 -0500

    Add code bases

---
+++ src/test/java/com/example/recipes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
